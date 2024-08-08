25581bfc767e7fe5ddfc0e875b3db27588b1e678
Vinay Kumar V C <84114504+VINUVARDHAN@users.noreply.github.com>
2024-08-08 20:58:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.use(session({
 }));
 app.use((req, res, next) => {
   console.log('Session ID:', req.sessionID);
+      console.log('Session Content:', req.session);
+
   next();
 });
 //-------------------------------------------------------------------------------------------------------------
