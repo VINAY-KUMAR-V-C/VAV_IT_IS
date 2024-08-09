d916f7a00d99829428e73b1373fb1544057f32a0
Vinay Kumar V C <84114504+VINUVARDHAN@users.noreply.github.com>
2024-08-09 22:44:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const utils = require('./utils')
+const app = express();
+const path = require('path');
+const { Pool } = require('pg');
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+//Notes :
+// make secure true once application is deployed in vercel
+//-------------------------------------------------------------------------------------------------------------
+const serverDetails = {
+  port: 6969,
+}
+
+//-------------------------------------------------------------------------------------------------------------
+const psql = new Pool({
+  connectionString: utils.database.psql.psqlConnectionData.postgresUrl,
+});
+//-------------------------------------------------------------------------------------------------------------
+// // Configure session middleware
+app.use(cookieParser());
+app.use(session({
+  resave : true,
+  saveUninitialized:true,
+  secret : "vav_it_is_XUV"
+}))
+// app.use(session({
+//   store: new pgSession({
+//     pool: psql,
+//     tableName: 'usersessions'
+//   }),
+//   // Custom serialization
+//   serialize: function (session) {
+//     return JSON.stringify(session); // Serialize to JSON string
+//   },
+//   unserialize: function (session) {
+//     return JSON.parse(session); // Deserialize from JSON string
+//   },
+//   secret: 'vav_it_is_XUV', // Replace with a strong, random secret key
+//   resave: false,
+//   saveUninitialized: false, // Recommend false to comply with laws that require permission before setting a cookie
+//   cookie: {
+//     secure: utils.isProduction,
+//     httpOnly: true,
+//     maxAge: 1000 * 60 * 30,
+//     sameSite: 'none'
+//   }
+// }));
+
+// app.use((req, res, next) => {
+//   console.log('Session ID:', req.sessionID);
+//   next();
+// });
+//-------------------------------------------------------------------------------------------------------------
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+//-------------------------------------------------------------------------------------------------------------
+app.set('views', path.join(__dirname, 'views'));
+//this is used to send value to html dynamically
+app.set('view engine', 'ejs');
+
+//-------------------------------------------------------------------------------------------------------------
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//-------------------------------------------------------------------------------------------------------------
+const companyDetails = utils.company;
+const companyRouterHandler = './' + companyDetails.folderName + '/' + companyDetails.routerFileName;
+const companyRouter = '/' + companyDetails.companyId;
+
+app.use(companyRouter, require(companyRouterHandler)(psql));
+app.get('/', (req, res) => {
+  res.redirect('/vav');
+});
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + '/views/404.html');
+});
+app.listen(serverDetails.port, () => {
+  console.log(`Server is running on ${utils.urls.baseURL}`);
+});
+//-------------------------------------------------------------------------------------------------------------
---
+++ utils.js
@@ -0,0 +1,48 @@
+path = require('path');
+utils = {};
+utils.isProduction = false;
+//-------------------------------------------------------------------------------------------------------------
+utils["urls"] = {
+    baseURL: utils.isProduction ? 'https://vav-it-is.vercel.app/' : 'http://localhost:6969/'
+}
+//-------------------------------------------------------------------------------------------------------------
+
+utils['database'] = {
+    psql: {
+        psqlConnectionData: {
+            postgresUrl: process.env.POSTGRES_URL,
+            postgresPrismaUrl: process.env.POSTGRES_PRISMA_URL,
+            postgresUrlNoSsl: process.env.POSTGRES_URL_NO_SSL,
+            postgresUrlNonPooling: process.env.POSTGRES_URL_NON_POOLING,
+            postgresUser: process.env.POSTGRES_USER,
+            postgresHost: process.env.POSTGRES_HOST,
+            postgresPassword: process.env.POSTGRES_PASSWORD,
+            postgresDatabase: process.env.POSTGRES_DATABASE
+        }
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+utils['apps'] = {
+    expenseTracker: {
+        id: 'expenseTracker',
+        label: 'Expense Tracker',
+        folderName: 'expenseTracker',
+        routerFileName: 'expenseTrackerRouter',
+        defaultModel: 'expense'
+    }
+}
+//-------------------------------------------------------------------------------------------------------------
+utils['company'] = {
+    companyId: 'vav',
+    appName: 'VAV',
+    folderName: 'vav',
+    routerFileName: 'vavRouter'
+}
+//-------------------------------------------------------------------------------------------------------------
+utils['session'] = {
+    timeOfLive: 120
+}
+//-------------------------------------------------------------------------------------------------------------
+
+module.exports = utils;
